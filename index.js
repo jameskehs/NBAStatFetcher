e8842726c5432765dd0e02f21872aed413f35db0
jameskehs <jameskehsdev@gmail.com>
2023-02-20 15:54:42 -0600

    Added plain JSON route

--- index.js
+++ index.js
@@ -10,7 +10,29 @@ app.get("/", (req, res, next) => {
     console.log(error);
   }
 });
-
+app.get("/lastGameJSON", async (req, res, next) => {
+  try {
+    const URL =
+      "https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=1&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2022-23&SeasonSegment=&SeasonType=Regular%20Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight=";
+    const config = { headers: { "Content-Type": "application/json", Referer: "https://www.nba.com/", "Cache-Control": "no-cache" } };
+    const {
+      data: { resultSets },
+    } = await axios.get(URL, config);
+    let statArray = [];
+    for (i = 0; i < resultSets[0].rowSet.length; i++) {
+      let player = new Object();
+      for (j = 0; j < resultSets[0].rowSet[i].length; j++) {
+        let key = resultSets[0].headers[j];
+        player[key] = resultSets[0].rowSet[i][j];
+      }
+      statArray.push(player);
+    }
+    res.json(statArray);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
 app.get("/lastGameStats", async (req, res, next) => {
   try {
     const URL =
@@ -21,11 +43,13 @@ app.get("/lastGameStats", async (req, res, next) => {
     } = await axios.get(URL, config);
     let tableHeaders = "";
     let tableBody = "";
+
     for (i = 0; i < resultSets[0].headers.length; i++) {
       tableHeaders += `<th>${resultSets[0].headers[i]}</th>`;
     }
+
     for (i = 0; i < resultSets[0].rowSet.length; i++) {
-      let rowData = "<tr>";
+      let rowData = "";
       for (j = 0; j < resultSets[0].rowSet[i].length; j++) {
         rowData += `<td>${resultSets[0].rowSet[i][j]}</td>`;
         if (j === resultSets[0].rowSet[i].length - 1) {
@@ -38,6 +62,7 @@ app.get("/lastGameStats", async (req, res, next) => {
     res.send(html);
   } catch (error) {
     console.log("ERROR", error);
+    res.send(error);
   }
 });
 
